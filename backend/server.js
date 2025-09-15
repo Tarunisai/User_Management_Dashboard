93b6b9d7887d7ac397c2a63516d314397f083f93
Tarunisai <109741671+Tarunisai@users.noreply.github.com>
2025-09-15 21:13:38 +0530

    Changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,12 @@ app.use(express.json());
 const userRoutes = require('./routes/users'); 
 app.use('/api/users', userRoutes);
 
-
 const buildPath = path.join(__dirname, 'build');
 app.use(express.static(buildPath));
 
-
-app.get('/:path*', (req, res) => {
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(buildPath, 'index.html'));
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
