a89f4c883e888fc414f7d556997bbb8772c17c13
Tarunisai <109741671+Tarunisai@users.noreply.github.com>
2025-09-15 20:19:01 +0530

    Changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/users');
 
 const app = express();
 
